4533ca0614893be69a97796c598d54d31e227255
Levi Gross <levi@levigross.com>
2016-02-12 15:12:52 -0500

    Init
    Signed-off-by: Levi Gross <levi@levigross.com>

--- base_get_test.go
+++ base_get_test.go
@@ -578,6 +578,52 @@ func TestGetBasicArgs(t *testing.T) {
 
 }
 
+func TestGetBasicArgsQueryStruct(t *testing.T) {
+	ro := &RequestOptions{
+		QueryStruct: struct {
+			Hello string `url:"Hello"`
+		}{
+			"World",
+		},
+	}
+	resp, _ := Get("http://httpbin.org/get?Goodbye=World", ro)
+
+	verifyOkArgsResponse(resp, t)
+
+}
+
+func TestGetBasicArgsQueryStructErr(t *testing.T) {
+	ro := &RequestOptions{
+		QueryStruct: 5,
+	}
+	resp, err := Get("http://httpbin.org/get?Goodbye=World", ro)
+
+	if err == nil {
+		t.Error("URL Parsing should have failed")
+	}
+
+	if resp.Ok == true {
+		t.Error("Request did return OK")
+	}
+
+}
+
+func TestGetBasicArgsErr(t *testing.T) {
+	ro := &RequestOptions{
+		Params: map[string]string{"Hello": "World"},
+	}
+	resp, err := Get("http://httpbin.org/get?Goodbye=%zzz", ro)
+
+	if err == nil {
+		t.Error("URL Parsing should have failed")
+	}
+
+	if resp.Ok == true {
+		t.Error("Request did return OK")
+	}
+
+}
+
 func TestGetBasicArgsParams(t *testing.T) {
 	ro := &RequestOptions{
 		Params: map[string]string{"Hello": "World", "Goodbye": "World"},
